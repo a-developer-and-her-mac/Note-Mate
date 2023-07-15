5cb43a51a66543a97e1cf7029e4b4b9eb1b66945
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2023-07-15 11:54:07 -0700

    add new UI

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
 })
 export class NoteCardComponent {
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
 
+  constructor() {}
+
+  ngOnInit() {}
 }
