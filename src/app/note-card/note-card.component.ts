b1c9e4ab549ab20d856482a7ba4adb16e9fbf8cf
Anna Cobb <64797765+a-developer-and-her-mac@users.noreply.github.com>
2023-07-15 19:12:40 +0000

    updates

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,14 +1,19 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss'],
 })
-export class NoteCardComponent {
-  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+export class NoteCardComponent implements OnInit {
+  @ViewChild('truncator') truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText!: ElementRef<HTMLElement>;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null); 
+    let viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+  }
 }
